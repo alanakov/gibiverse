1e529ea8b558dd3ce93784bbf8a3fa5853c937bb
alanakov <angelikovalski@gmail.com>
2025-03-24 22:15:42 -0300

    feat: add collection model

---
+++ backend/models/CollectionModel.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+import ComicBookModel from "./ComicBookModel";
+
+class CollectionModel extends Model {
+  comicBookId: number | undefined;
+  name: string | undefined;
+  description: string | undefined;
+  writerId: number | undefined;
+}
+
+CollectionModel.init(
+  {
+    collectionId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    writerId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "CollectionModel",
+    tableName: "collections",
+  }
+);
+
+CollectionModel.hasMany(ComicBookModel, { foreignKey: "collectionId" });
+ComicBookModel.belongsTo(CollectionModel, { foreignKey: "collectionId" });
+
+export default CollectionModel;
