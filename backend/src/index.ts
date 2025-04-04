9cfc134f98c32ff2a04d4bcaf260c6d58a1e8b6a
alanakov <angelikovalski@gmail.com>
2025-04-04 15:50:17 -0300

    feat: connection with database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,31 @@
 import express from "express";
+import cors from "cors";
 import userRouter from "./routes/UserRoutes";
 import genreRouter from "./routes/GenreRoutes";
 import comicBookRouter from "./routes/ComicBookRoutes";
 import collectionRouter from "./routes/CollectionRoutes";
 import authorRouter from "./routes/AuthorRoutes";
+import sequelize from "./config/database";
 
 const app = express();
 const port = 3000;
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Database synced successfully!");
+  })
+  .catch((error) => {
+    console.error("Error syncing database:", error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Initial route");
 });
 
-// Configuração para ler JSON no corpo da requisição
 app.use(express.json());
 
-// Importando e usando as rotas
+app.use(cors());
 app.use(userRouter);
 app.use(genreRouter);
 app.use(comicBookRouter);
