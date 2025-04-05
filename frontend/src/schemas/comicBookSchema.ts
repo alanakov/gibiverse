6785eef283ddac9ce9cab5f3e30f0e615801437e
alanakov <angelikovalski@gmail.com>
2025-04-05 17:15:40 -0300

    feat: add comic book schema and type definitions

---
+++ frontend/src/schemas/comicBookSchema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const createComicBookSchema = z.object({
+  title: z.string().min(1, "Título é obrigatório"),
+  description: z.string(),
+  authorId: z.number().min(1, "O autor é obrigatório"),
+  coverUrl: z
+    .string()
+    .url("URL da imagem de capa inválida")
+    .min(1, "URL da imagem de capa é obrigatória"),
+});
+
+export type CreateComicBookSchemaType = z.infer<typeof createComicBookSchema>;
+
+export const editComicBookSchema = createComicBookSchema;
+
+export type EditComicBookSchemaType = z.infer<typeof editComicBookSchema>;
---
+++ frontend/src/types/comicBook.ts
@@ -0,0 +1,7 @@
+export interface ComicBook {
+  id: number;
+  title: string;
+  description: string;
+  authorId: number;
+  coverUrl: string;
+}
