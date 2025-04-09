4a10572ca1039b21318c9406815016f3a43eb202
alanakov <angelikovalski@gmail.com>
2025-04-09 08:29:46 -0300

    feat: add authMiddleware to all routes in Author, Collection, ComicBook, and Genre routers for enhanced security

--- backend/src/routes/ComicBookRoutes.ts
+++ backend/src/routes/ComicBookRoutes.ts
@@ -10,15 +10,10 @@ import { authMiddleware } from "../middleware/authMiddleware";
 
 const comicBookRouter = express.Router();
 
-// comicBookRouter.post("/comicbooks", authMiddleware, createComicBook);
-// comicBookRouter.get("/comicbooks", authMiddleware, getAllComicBooks);
-// comicBookRouter.get("/comicbooks/:id", authMiddleware, getComicBookById);
-// comicBookRouter.put("/comicbooks/:id", authMiddleware, updateComicBook);
-// comicBookRouter.delete("/comicbooks/:id", authMiddleware, destroyComicBookById);
-comicBookRouter.post("/comicbooks", createComicBook);
-comicBookRouter.get("/comicbooks", getAllComicBooks);
-comicBookRouter.get("/comicbooks/:id", getComicBookById);
-comicBookRouter.put("/comicbooks/:id", updateComicBook);
-comicBookRouter.delete("/comicbooks/:id", destroyComicBookById);
+comicBookRouter.post("/comicbooks", authMiddleware, createComicBook);
+comicBookRouter.get("/comicbooks", authMiddleware, getAllComicBooks);
+comicBookRouter.get("/comicbooks/:id", authMiddleware, getComicBookById);
+comicBookRouter.put("/comicbooks/:id", authMiddleware, updateComicBook);
+comicBookRouter.delete("/comicbooks/:id", authMiddleware, destroyComicBookById);
 
 export default comicBookRouter;
