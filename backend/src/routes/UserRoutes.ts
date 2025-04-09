cdc6058c18ae6454423607a4360850910dbcb010
alanakov <angelikovalski@gmail.com>
2025-04-09 11:05:16 -0300

    feat: implement login functionality with API integration and error handling

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -6,11 +6,13 @@ import {
   getUserById,
   updateUser,
 } from "../controllers/UserController";
+import { loginUsuario } from "../controllers/LoginController";
 import { authMiddleware } from "../middleware/authMiddleware";
 
 const userRouter = express.Router();
 
 userRouter.post("/users", createUser);
+userRouter.post("/login", loginUsuario);
 
 userRouter.get("/users", authMiddleware, getAllUsers);
 userRouter.get("/users/:id", authMiddleware, getUserById);
