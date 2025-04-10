4f331e8dfbd37c454daade198e75c6f8283a7bf7
alanakov <angelikovalski@gmail.com>
2025-04-10 11:38:50 -0300

    refactor: remove console.log

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -17,23 +17,19 @@ export const authMiddleware = (
   const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
-    console.error("Token não fornecido.");
     return res.status(401).json({ error: "Access denied. No token provided" });
   }
 
   try {
-    const decoded: any = verifyToken(token); // Decodifica o token JWT
-    console.log("Token decodificado:", decoded); // Log para verificar o token decodificado
+    const decoded: any = verifyToken(token);
 
     if (!decoded || !decoded.id) {
-      console.error("Estrutura do token inválida:", decoded);
       return res.status(401).json({ error: "Invalid token structure" });
     }
 
-    req.user = decoded; // Armazena o usuário decodificado em `req.user`
+    req.user = decoded;
     next();
   } catch (error) {
-    console.error("Erro ao validar o token:", error);
     return res.status(401).json({ error: "Access denied. Invalid token" });
   }
 };
